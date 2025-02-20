bfa5f9ec6bc3664269fa836d42d9335fcaae2418
EC2 Default User <ec2-user@ip-172-31-5-203.ap-northeast-1.compute.internal>
2025-02-20 02:19:43 +0000

    bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+    before_action :is_matching_login_user, only: [:edit, :update]
+
     def index
         @book = Book.new
         @books = Book.all
@@ -49,6 +51,13 @@ class BooksController < ApplicationController
 
     def book_params
       params.require(:book).permit(:title, :body)
+    end
+
+    def is_matching_login_user
+        user = User.find(params[:id])
+        unless user.id == current_user.id
+          redirect_to books_path
+        end
     end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  include ActionView::Helpers::TextHelper
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
     @user = current_user
@@ -33,5 +33,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
 
 end
